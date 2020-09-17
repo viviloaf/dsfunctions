b0b9f1bd85c6f5fad45f8d89c7a1cdd24471160b
Vivian Nguyen <ngux.vinh@gmail.com>
2020-09-17 11:05:52 -0400

    add prep for dataframe functions

---
+++ dsfunctions/prepdata.py
@@ -0,0 +1,15 @@
+class prepdata:
+
+    def __init__:
+        pass
+
+    def variable_setup(self, df):
+        '''
+        input dataframe
+        break into X and y
+
+        pass in columns
+        '''
+        pass
+
+    
