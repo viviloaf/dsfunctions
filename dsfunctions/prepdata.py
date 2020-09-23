31660e010edad7b5f351b6cedde1716cff4868fc
Vivian Nguyen <ngux.vinh@gmail.com>
2020-09-23 16:55:22 -0400

    write more cohesive docstring

--- dsfunctions/prepdata.py
+++ dsfunctions/prepdata.py
@@ -1,15 +1,19 @@
 class prepdata:
+    '''
+    This class is meant for numerical and cateorical data preprocessing as well as initial data visualization
+    
+    todo
+    
+    write a function that displays null values, fragmentation of the data (not null / all data), and information from the describe
+
+        sort to show the biggest discrepnicies first, such as smallest number, largest number, or a threshold of fragmentation (like 50% of data is null)
+
+        mark all columns in the predictor which are null as candidates to remove
+    '''
 
     def __init__:
         pass
 
-    def variable_setup(self, df):
-        '''
-        input dataframe
-        break into X and y
-
-        pass in columns
-        '''
-        pass
+    
 
     
