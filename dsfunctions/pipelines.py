3b989f8a5691ed12d755737d4e150f56fea94e28
Vivian Nguyen <ngux.vinh@gmail.com>
2020-09-23 16:54:03 -0400

    rename

---
+++ dsfunctions/pipelines.py
@@ -0,0 +1,12 @@
+class pipelines:
+    '''
+    To start keep it simple
+    then consider an exhaustive grid search
+    
+    Import my models.py when I am finished with it from project 2 for a suite of functions
+    that deal with modeling data
+
+
+    add a preprocessing module to define it, or to use a user defined preprocessing model,
+    allow user to pass variable, but also have self.preprocessing as the normal one for numerical and categeorical data
+    '''
