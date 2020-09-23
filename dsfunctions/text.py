ce43295f273f0009f10d41ad98e5dd0816bb3607
Vivian Nguyen <ngux.vinh@gmail.com>
2020-09-23 16:54:21 -0400

    add NLP module

---
+++ dsfunctions/text.py
@@ -0,0 +1,9 @@
+class text:
+    '''
+    This is a suite of functions for Natural Language processing
+    '''
+
+    def text_scraper_cleaner():
+        '''
+        cleans text scraped from html for NLP
+        '''
